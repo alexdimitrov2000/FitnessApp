d2fe8a3403477d10a9fc56d53b72a77c57a80599
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2018-12-18 22:34:08 +0200

    authorize test

---
+++ FitnessApp/FitnessApp.Web/Controllers/PostsController.cs
@@ -0,0 +1,14 @@
+﻿namespace FitnessApp.Web.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class PostsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
