47c5cece6635ccc8b936528a45b5a6897df4d8eb
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2018-12-18 23:30:05 +0200

    Added initial post create.

---
+++ FitnessApp/FitnessApp.Web/Models/Posts/PostCreateInputModel.cs
@@ -0,0 +1,15 @@
+﻿namespace FitnessApp.Web.Models.Posts
+{
+    using Microsoft.AspNetCore.Http;
+
+    using System.ComponentModel.DataAnnotations;
+
+    public class PostCreateInputModel
+    {
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public IFormFile Image { get; set; }
+    }
+}
