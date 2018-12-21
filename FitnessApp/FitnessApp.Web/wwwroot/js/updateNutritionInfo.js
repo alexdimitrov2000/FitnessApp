b006898ede2b6799faa8d60806f24771557c76dd
Tomas Tomov <tomastomov@yahoo.com>
2018-12-21 22:20:32 +0200

    Added updating on the food values when the weight is changed.

--- FitnessApp/FitnessApp.Web/wwwroot/js/updateNutritionInfo.js
+++ FitnessApp/FitnessApp.Web/wwwroot/js/updateNutritionInfo.js
@@ -1,7 +1,11 @@
-﻿$('.update-item').click(function update() {
+﻿let initialCalories;
+let initialProtein;
+let initialCarbohydrates;
+let initialFats;
+$('.update-item').click(function update() {
     event.preventDefault();
     let baseUrl = "http://localhost:5000"
-
+    
     $.ajax({
         url: baseUrl + $(this).attr('href'),
         method: "GET",
@@ -11,14 +15,37 @@
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
-        async: false,
         dataType: 'json',
         success: function (response) {
+            $('#foodName').text(response.name);
             $('#calories').text(response.calories);
             $('#protein').text(response.protein + 'g');
             $('#carbs').text(response.carbohydrates + 'g');
             $('#fats').text(response.fats + 'g');
+            $('#multiplier').val(1);
+            initialCalories = parseFloat(response.calories);
+            initialProtein = parseFloat(response.protein);
+            initialCarbohydrates = parseFloat(response.carbohydrates);
+            initialFats = parseFloat(response.fats);
         }
     })
     
 });
+
+$('#multiplier').on('input', function changeValues() {
+    let multiplier = parseFloat($('#multiplier').val());
+
+    if (multiplier <= 0 || isNaN(multiplier) || !initialCalories) {
+        $('#calories').text(0);
+        $('#protein').text(0 + 'g');
+        $('#carbs').text(0 + 'g');
+        $('#fats').text(0 + 'g');
+    }
+    else {
+        $('#calories').text(initialCalories * multiplier);
+        $('#protein').text(initialProtein * multiplier + 'g');
+        $('#carbs').text(initialCarbohydrates * multiplier + 'g');
+        $('#fats').text(initialFats * multiplier + 'g');
+    }
+    
+})
