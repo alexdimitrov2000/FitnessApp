635de03e8a63f23549da97184c0bb852a7c5bda3
Tomas Tomov <tomastomov@yahoo.com>
2019-01-12 23:21:34 +0200

    Added functionality for the Diary in FoodsController

--- FitnessApp/FitnessApp.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ FitnessApp/FitnessApp.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -79,11 +79,16 @@
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var user = await this.userManager.FindByNameAsync(Input.Username);
-                if (!user.IsActive)
+
+                if(user != null)
                 {
-                    ModelState.AddModelError(string.Empty, "Your account has been deactivated!");
-                    return Page();
+                    if (!user.IsActive)
+                    {
+                        ModelState.AddModelError(string.Empty, "Your account has been deactivated!");
+                        return Page();
+                    }
                 }
+                
                 var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
