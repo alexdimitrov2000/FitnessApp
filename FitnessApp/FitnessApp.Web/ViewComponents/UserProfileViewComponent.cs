b471ba46ebae287a94c0f28dca3a6a6fab3f2449
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2019-01-13 00:31:34 +0200

    Added visualization of the profile picture on the navbar next to the user's username.

---
+++ FitnessApp/FitnessApp.Web/ViewComponents/UserProfileViewComponent.cs
@@ -0,0 +1,33 @@
+﻿namespace FitnessApp.Web.ViewComponents
+{
+    using FitnessApp.Models;
+    using FitnessApp.Services.Contracts;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Identity;
+
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    [ViewComponent(Name = "UserProfile")]
+    public class UserProfileViewComponent : ViewComponent
+    {
+        private readonly UserManager<FitnessUser> userManager;
+        private readonly ICloudinaryService cloudinaryService;
+
+        public UserProfileViewComponent(UserManager<FitnessUser> userManager, ICloudinaryService cloudinaryService)
+        {
+            this.userManager = userManager;
+            this.cloudinaryService = cloudinaryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(ClaimsPrincipal principalUser)
+        {
+            var user = await this.userManager.GetUserAsync(principalUser);
+
+            var profilePictureUrl = this.cloudinaryService.BuildPictureUrl(user.ProfilePicture);
+
+            return this.View((object)profilePictureUrl);
+        }
+    }
+}
